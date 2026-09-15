baa5a20db26bf4da1006eeaa0fe7d3399f7e2dce
Eduardo Serrano <eduardorojoserrano99@gmail.com>
2026-09-15 10:18:29 -0300

    Día 3

---
+++ 01-fundamentos-comparados/arreglos.js
@@ -0,0 +1,20 @@
+//solicitudes
+
+
+// Fundamentos comparados - Arreglos y bucles en JavaScript
+
+const solicitudes = ["___", "___", "___", "___"];
+
+// predigo: ___
+console.log(solicitudes);
+// real: ___
+
+// el primero
+// predigo: ___
+console.log(solicitudes[0]);
+// real: ___
+
+// el último
+// predigo: ___
+console.log(solicitudes[solicitudes.length - 1]);
+// real: ___
