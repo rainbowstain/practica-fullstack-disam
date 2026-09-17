d995526f1c6d21c49cfab4c4571fbb868725dc5a
Eduardo Rojo Serrano <eduardorojoserrano99@gmail.com>
2026-09-16 21:03:42 -0300

    listas py

--- 01-fundamentos-comparados/py/listas.py
+++ 01-fundamentos-comparados/py/listas.py
@@ -14,3 +14,17 @@ for i in range(len(montos)):
 montos2 = [10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000]
 for lista in montos2:
     print(lista)
+
+mayores = []
+
+for monto in montos2:
+    if monto > 10000:
+        mayores.append(monto)
+print(f"Montos originales: {montos2}")
+print(f"Montos mayores a 10000: {mayores}") 
+print(f"Cantidad: {len(mayores)}")
+for mayore in mayores:
+    if mayore > 50000:
+        print(f"{mayore} - director")
+    elif mayore > 10000:
+        print(f"{mayore} - jefe")
